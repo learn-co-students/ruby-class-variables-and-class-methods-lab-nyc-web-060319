78060c0ccf633f8cbca1472283e760c50c43c4a5
Alexander Riccio <test35965+learn@gmail.com>
2019-06-04 17:45:26 -0400

    mostly solve lab

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+require "pry"
+
+class Song
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @@artists << artist
+        @genre = genre
+        @@genres << genre
+        @@count += 1
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.uniq.length
+    end
+
+    def self.artist_count
+        @@artists.uniq.length
+    end
+    
+end
