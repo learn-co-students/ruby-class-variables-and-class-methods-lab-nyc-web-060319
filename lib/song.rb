540a2ec0c72e4d6bfe3120aa5543c920a598ae90
Alexander Riccio <test35965+learn@gmail.com>
2019-06-04 17:50:34 -0400

    finish solving lab

--- lib/song.rb
+++ lib/song.rb
@@ -29,11 +29,27 @@ class Song
     end
 
     def self.genre_count
-        @@genres.uniq.length
+        genre_hash = {}
+        @@genres.each do |genre|
+            if genre_hash.has_key?(genre)
+                genre_hash[genre] += 1
+            else
+                genre_hash[genre] = 1
+            end
+        end
+        genre_hash
     end
 
     def self.artist_count
-        @@artists.uniq.length
+        artist_hash = {}
+        @@artists.each do |artist|
+            if artist_hash.has_key?(artist)
+                artist_hash[artist] += 1
+            else
+                artist_hash[artist] = 1
+            end
+        end
+        artist_hash
     end
     
 end
